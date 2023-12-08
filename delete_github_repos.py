c87e183ac6c5e39033496ee522302070394a9e3c
Mickael <40262911+Mik3asg@users.noreply.github.com>
2023-12-08 13:57:00 +0000

    python script updated with comments

--- delete_github_repos.py
+++ delete_github_repos.py
@@ -1,12 +1,17 @@
 '''
-Script Purpose: Deletes specified GitHub repositories using PyGithub,
+Script Purpose:
+Deletes specified GitHub repositories using PyGithub,
 utilising GitHub username and personal access token from environment variables (GITHUB_USERNAME and GITHUB_TOKEN).
 
-Note: Ensure environment variables GITHUB_USERNAME and GITHUB_TOKEN are set with your GitHub credentials.
-    Usage: python delete_github_repos.py -r REPO_NAME [REPO_NAME ...]
-        -r, --repos      List of repository names to delete
+Pre-requisites:
+To run the script successfully, you'll need to install the PyGithub library
+and possibly the python-dotenv library if you plan to use environment variables for your credentials.
 
-Note: If the environment variables are not defined, specify the username and token using -u and -t options respectively.
+Note-1: Ensure environment variables GITHUB_USERNAME and GITHUB_TOKEN are set with your GitHub credentials.
+Usage: python delete_github_repos.py -r REPO_NAME [REPO_NAME ...]
+    -r, --repos      List of repository names to delete
+
+Note-2: If the environment variables are not defined, specify the username and token using -u and -t options respectively.
     Usage: delete_github_repos.py [-h] [-u USERNAME] [-t TOKEN] -r REPOS [REPOS ...]
 '''
 import os
@@ -26,9 +31,9 @@ def delete_repositories(username, access_token, repos_to_delete):
             print(f"An error occurred while deleting '{repo_name}': {e}")
 
 def main():
-    parser = argparse.ArgumentParser(description='Delete GitHub repositories.')                                           # Set environment variables:
-    parser.add_argument('-u', '--username', default=os.getenv('GITHUB_USERNAME'), help='Your GitHub username')            # set GITHUB_USERNAME=YourUsername
-    parser.add_argument('-t', '--token', default=os.getenv('GITHUB_TOKEN'), help='Your GitHub personal access token')     # set GITHUB_TOKEN=YourAccessToken
+    parser = argparse.ArgumentParser(description='Delete GitHub repositories.')
+    parser.add_argument('-u', '--username', default=os.getenv('GITHUB_USERNAME'), help='Your GitHub username')
+    parser.add_argument('-t', '--token', default=os.getenv('GITHUB_TOKEN'), help='Your GitHub personal access token')
     parser.add_argument('-r', '--repos', nargs='+', required=True, help='List of repository names to delete')
 
     args = parser.parse_args()
